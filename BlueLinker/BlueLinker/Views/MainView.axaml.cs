38ea263bb07a21106c01e0360f31a9f871ebef34
sergeev.d <sergeev.d@bk-s.pro>
2024-11-04 12:22:34 +0300

    Добавьте файлы проекта.

---
+++ BlueLinker/BlueLinker/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BlueLinker.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
